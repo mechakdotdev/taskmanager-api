6e9c65498df4b0fbe68b42c0d4aba8d4afb04252
Mechak Holondo <mechakand1@gmail.com>
2025-05-25 10:59:49 +0100

    Rename TaskController to the more accurate name of SchedulerController

--- src/main/java/taskmanager/api/controller/SchedulerController.java
+++ src/main/java/taskmanager/api/controller/SchedulerController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/schedule")
 @RequiredArgsConstructor
-public class TaskController {
+public class SchedulerController {
 
     private final SchedulerService schedulerService;
     private final TaskMapper taskMapper;

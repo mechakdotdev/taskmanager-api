c87c595c296dcc4c511b7f446ab05fc83bceb82e
Mechak Holondo <mechakand1@gmail.com>
2025-05-27 14:34:55 +0100

    bug-fix: Implement concrete mock tasks - assignedByUser is a required field that is validated within the controller so tests will fail without concrete mock implementation

--- src/test/java/taskmanager/api/service/SchedulerServiceTests.java
+++ src/test/java/taskmanager/api/service/SchedulerServiceTests.java
@@ -7,18 +7,19 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.Mock;
 
+import taskmanager.api.model.Priority;
 import taskmanager.api.model.Task;
 import taskmanager.api.repository.TaskRepository;
 import taskmanager.api.scheduler.Scheduler;
 import taskmanager.api.scheduler.SchedulerResolver;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-import static taskmanager.api.utils.TestHelper.getAllAlgorithmNames;
-import static taskmanager.api.utils.TestHelper.mockUser;
+import static taskmanager.api.utils.TestHelper.*;
 
 @ExtendWith(MockitoExtension.class)
 class SchedulerServiceTests {
@@ -45,12 +46,12 @@ class SchedulerServiceTests {
 
         // Arrange
         var mockUserId = mockUser().getId();
-        var mockTasks = List.of(new Task());
-        var scheduledTasks = List.of(new Task());
+        var tasks = mockTasks();
+        var scheduledTasks = mockTasks();
 
-        when(taskRepository.findAll()).thenReturn(mockTasks);
+        when(taskRepository.findAll()).thenReturn(tasks);
         when(schedulerResolver.resolve(algorithm)).thenReturn(mockScheduler);
-        when(mockScheduler.schedule(mockTasks)).thenReturn(scheduledTasks);
+        when(mockScheduler.schedule(tasks)).thenReturn(scheduledTasks);
 
         // Act
         var result = schedulerService.schedule(algorithm, mockUserId);
@@ -59,7 +60,16 @@ class SchedulerServiceTests {
         assertEquals(scheduledTasks, result);
         verify(taskRepository, times(1)).findAll();
         verify(schedulerResolver, times(1)).resolve(algorithm);
-        verify(mockScheduler, times(1)).schedule(mockTasks);
+        verify(mockScheduler, times(1)).schedule(tasks);
+    }
+
+    private List<Task> mockTasks() {
+        var mockDeadline = LocalDateTime.of(1990, 1, 1, 0, 0);
+
+        var mockTask1 = mockTask("Task 1", Priority.LOW, 2, mockDeadline);
+        var mockTask2 = mockTask("Task 2", Priority.MEDIUM, 3, mockDeadline);
+
+        return List.of(mockTask1, mockTask2);
     }
 
     static Stream<String> allAlgorithmNames() { return getAllAlgorithmNames(); }
